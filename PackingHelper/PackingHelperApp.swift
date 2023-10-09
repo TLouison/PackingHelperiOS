6a0c937607cb3c074450d1d7784d9468a7330a1a
TLouison <toddmlouison@gmail.com>
2023-10-09 00:19:28 -0400

    Initial Commit

---
+++ PackingHelper/PackingHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PackingHelperApp.swift
+//  PackingHelper
+//
+//  Created by Todd Louison on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct PackingHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
